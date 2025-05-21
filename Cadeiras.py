498171dced555463529a9b6dc43e8418dca82d39
João Carlos Rodrigues da Silva <151682037+JoaoCarlos12@users.noreply.github.com>
2025-05-21 08:22:09 -0300

    JogoAtualizado

--- Cadeiras.py
+++
@@ -1,8 +0,0 @@
-quantidade = int(input('Digite a quantidade de lugares necessario:'))
-
-if(quantidade >= 80):
-    print('Você tera que utilizar o auditorio alpha')
-elif(quantidade < 80 & quantidade >=1):
-    print('Utilize o auditorio Beta')
-else:
-    print('numero invalido')
